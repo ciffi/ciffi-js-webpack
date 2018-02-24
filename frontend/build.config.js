8986f4d43ded6c5046a53fcbf6be18d373e8d5c9
ciffi <alessio@ciffi.it>
2018-02-24 16:18:09 +0100

    added some loader for webpack - v0.18.1

--- frontend/build.config.js
+++ frontend/build.config.js
@@ -4,61 +4,63 @@ const path = require('path');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 module.exports = {
-	entry: {
-		main: './' + ConfigFile.srcPathName + '/scripts/main.js'
-	},
-	output: {
-		path: __dirname + '/' + ConfigFile.assetsPath,
-		publicPath: ConfigFile.publicPath,
-		filename: '[name].js',
-		chunkFilename: '[name].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				loader: 'eslint-loader',
-				exclude: [/(node_modules)/, __dirname + '/' + 'src/scripts/vendors'],
-				options: {
-					configFile: './.eslintrc'
-				}
-			}, {
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
+    entry: {
+        main: './' + ConfigFile.srcPathName + '/scripts/main.js'
+    },
+    output: {
+        path: __dirname + '/' + ConfigFile.assetsPath,
+        publicPath: ConfigFile.publicPath,
+        filename: '[name].js',
+        chunkFilename: '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.js$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                exclude: [/(node_modules)/, __dirname + '/' + 'src/scripts/vendors'],
+                options: {
+                    configFile: './.eslintrc'
+                }
+            }, {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
                         presets: ['env', 'react', 'es2015', 'stage-0']
-					}
-				}
-			}, {
-				test: /\.jsx$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env', 'react', 'es2015', 'stage-0']
-					}
-				}
-			}, {
-				test: /\.twig$/,
-				loader: 'twig-loader'
-			}
-		]
-	},
-	resolve: {
-		alias: {
-			Config: path.resolve(__dirname, ConfigFile.srcPathName + '/scripts/config/config.js'),
-		}
-	},
-	plugins: [
-		new HardSourceWebpackPlugin(),
+                    }
+                }
+            }, {
+                test: /\.jsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react', 'es2015', 'stage-0']
+                    }
+                }
+            }, {
+                test: /\.twig$/,
+                loader: 'twig-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            Config: path.resolve(__dirname, ConfigFile.srcPathName + '/scripts/config/config.js'),
+        }
+    },
+    plugins: [
+        new HardSourceWebpackPlugin(),
         new UglifyJSPlugin({
             uglifyOptions: {
                 sourceMap: false,
                 comments: false
             }
         })
-	]
+    ]
 };
--- frontend/dev.config.js
+++ frontend/dev.config.js
@@ -5,60 +5,62 @@ const path = require('path');
 const HardSourceWebpackPlugin = require('hard-source-webpack-plugin');
 
 module.exports = {
-	entry: {
-		main: './' + ConfigFile.srcPathName + '/scripts/main.js'
-	},
-	output: {
-		publicPath: ConfigFile.publicPath,
-		path: __dirname + '/' + ConfigFile.assetsPath,
-		filename: '[name].js',
-		chunkFilename: '[name].js'
-	},
-	devtool: 'source-map',
-	watch: true,
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				enforce: 'pre',
-				loader: 'eslint-loader',
-				exclude: [/(node_modules)/, __dirname + '/' + 'src/scripts/vendors'],
-				options: {
-					configFile: './.eslintrc'
-				}
-			}, {
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
+    entry: {
+        main: './' + ConfigFile.srcPathName + '/scripts/main.js'
+    },
+    output: {
+        publicPath: ConfigFile.publicPath,
+        path: __dirname + '/' + ConfigFile.assetsPath,
+        filename: '[name].js',
+        chunkFilename: '[name].js'
+    },
+    devtool: 'source-map',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }, {
+                test: /\.js$/,
+                enforce: 'pre',
+                loader: 'eslint-loader',
+                exclude: [/(node_modules)/, __dirname + '/' + 'src/scripts/vendors'],
+                options: {
+                    configFile: './.eslintrc'
+                }
+            }, {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
                         presets: ['env', 'react', 'es2015', 'stage-0']
-					}
-				}
-			}, {
-				test: /\.jsx$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['env', 'react', 'es2015', 'stage-0']
-					}
-				}
-			}, {
-				test: /\.twig$/,
-				loader: 'twig-loader'
-			}
-		]
-	},
-	resolve: {
-		alias: {
-			Config: path.resolve(__dirname, ConfigFile.srcPathName + '/scripts/config/config.js'),
-		}
-	},
-	plugins: [
-		new HardSourceWebpackPlugin(),
-		new OpenBrowserPlugin({
-			url: _indexUrl
-		})
-	]
+                    }
+                }
+            }, {
+                test: /\.jsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env', 'react', 'es2015', 'stage-0']
+                    }
+                }
+            }, {
+                test: /\.twig$/,
+                loader: 'twig-loader'
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            Config: path.resolve(__dirname, ConfigFile.srcPathName + '/scripts/config/config.js'),
+        }
+    },
+    plugins: [
+        new HardSourceWebpackPlugin(),
+        new OpenBrowserPlugin({
+            url: _indexUrl
+        })
+    ]
 };
